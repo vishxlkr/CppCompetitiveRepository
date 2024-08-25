031ab4ca426d0c6b3963f8ca2c11e03dbffe61ce
vishxlkr <143512151+vishxlkr@users.noreply.github.com>
2024-08-25 16:20:08 +0530

    Update pacific_atlantic_water_flow.cpp

--- graph_lectures/pacific_atlantic_water_flow.cpp
+++ graph_lectures/pacific_atlantic_water_flow.cpp
@@ -10,6 +10,30 @@ public:
     int cols;
     vector<vector<int> > h;
     
+    
+    
+    vector<vector<bool> > bfs(queue<pair<int, int> > &qu) {
+        vector<vector<bool> > visited(rows, vector<bool> (cols, false));
+        while(not qu.empty()) {
+            auto cell = qu.front();
+            qu.pop();
+            int i = cell.first;
+            int j = cell.second;
+            visited[i][j] = true;
+            for(int d = 0; d < 4; d++) {
+                int newRow = i+dir[d][0];
+                int newCol = j+dir[d][1];
+                if(newRow < 0 or newCol < 0 or newRow >= rows or newCol >= cols) continue; // you exited the grid
+                if(visited[newRow][newCol]) continue;
+                if(h[newRow][newCol] < h[i][j]) continue; // h[newRow][newCol] -> neighbours height, h[i][j] -> curr cell's heigh
+                qu.push({newRow, newCol});
+            }
+        }
+        return visited;
+    }
+
+
+
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         rows = heights.size();
         cols = heights[0].size();
@@ -41,26 +65,6 @@ public:
             }
         }
         return result;
-    }
-    
-    vector<vector<bool> > bfs(queue<pair<int, int> > &qu) {
-        vector<vector<bool> > visited(rows, vector<bool> (cols, false));
-        while(not qu.empty()) {
-            auto cell = qu.front();
-            qu.pop();
-            int i = cell.first;
-            int j = cell.second;
-            visited[i][j] = true;
-            for(int d = 0; d < 4; d++) {
-                int newRow = i+dir[d][0];
-                int newCol = j+dir[d][1];
-                if(newRow < 0 or newCol < 0 or newRow >= rows or newCol >= cols) continue; // you exited the grid
-                if(visited[newRow][newCol]) continue;
-                if(h[newRow][newCol] < h[i][j]) continue; // h[newRow][newCol] -> neighbours height, h[i][j] -> curr cell's heigh
-                qu.push({newRow, newCol});
-            }
-        }
-        return visited;
     }
 };
 int main() {
